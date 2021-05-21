db2e9cfa52b7e77f0f4818f943cf568d08dbff8e
Jose Starsky Naya Lara <starsky@DESKTOP-6RPT0A8.home>
2021-05-21 09:08:03 +0200

    update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -10,25 +10,107 @@ const Dashboard = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    if (dashboardData) console.log(dashboardData);
+  }, [dashboardData]);
+
+  const findVersions = () => {
+    const resultsVersion = dashboardData.results.map((i) => i.package.version);
+    const filterVersions = resultsVersion.filter(
+      (i, index) => resultsVersion.indexOf(i) === index
+    );
+    return filterVersions;
+  };
+
+  const sortVersions = () =>
+    findVersions().sort((a, b) => {
+      if (a === b) {
+        return 0;
+      }
+      const aVersion = a.split(".");
+      const bVersion = b.split(".");
+      for (let i = 0; i < Math.max(aVersion.length, bVersion.length); i++) {
+        if (Number(aVersion[i]) >= Number(bVersion[i])) return -1;
+        if (parseInt(aVersion[i]) <= parseInt(bVersion[i])) return 1;
+      }
+      return 0;
+    });
+
+  const totalMaintainers = (array) => {
+    return array.results.reduce((total, i) => {
+      const maintainersAmount = i.package.maintainers.length;
+      return total + maintainersAmount;
+    }, 0);
+  };
+
+  const totalManteiners = () =>{
+    
+  }
+
   return (
     <div style={{ paddingLeft: "5rem" }}>
-      <h2>Dashboard</h2>
-      <div>
+      <div style={{ display: "flex", flexWrap: "nowrap" }}>
         {dashboardData && (
           <>
-            <h3>{`Totla: ${dashboardData.total}`}</h3>
-            {dashboardData.results.map((i, index) => (
-              <div
-                key={index}
-                style={{
-                  paddingBottom: "1rem",
-                  color: `${i.searchScore < 32 ? "red" : "green"}`,
-                }}
-              >
-                <div>{`name: ${i.package.name}`}</div>
-                <div>{`score: ${i.searchScore}`}</div>
+            <div style={{ paddingRight: "2rem" }}>
+              <h2>Dashboard</h2>
+              <div>
+                <h3>{`Totla: ${dashboardData.total}`}</h3>
+                {dashboardData.results.map((i, index) => (
+                  <div
+                    key={index}
+                    style={{
+                      paddingBottom: "1rem",
+                      color: `${i.searchScore < 32 ? "red" : "green"}`,
+                    }}
+                  >
+                    <div>{`name: ${i.package.name}`}</div>
+                    <div>{`score: ${i.searchScore}`}</div>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
+            <div style={{ paddingRight: "2rem" }}>
+              <h2>Data highlights (sort and loop)</h2>
+              <div>
+                <h3>Package sort versions (Sort)</h3>
+                {sortVersions().map((i, index) => (
+                  <div
+                    key={index}
+                    style={{
+                      paddingBottom: "1rem",
+                      color: `${i.searchScore < 32 ? "red" : "green"}`,
+                    }}
+                  >
+                    <div>{` App Sort version: ${i}`}</div>
+                  </div>
+                ))}
+                <h3>Package versions (foreach || map )</h3>
+                {findVersions().map((i, index) => (
+                  <div
+                    key={index}
+                    style={{
+                      paddingBottom: "1rem",
+                    }}
+                  >
+                    <div>{`App version: ${i}`}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div style={{ paddingRight: "2rem" }}>
+              <h2>Data highlights (reduce)</h2>
+              <div>
+                <h3>Mateiners (Sort)</h3>
+                <div
+                  style={{
+                    paddingBottom: "1rem",
+                  }}
+                >
+                  <div>{`Amount of Maintainers: ${totalMaintainers(dashboardData)}`}</div>
+                </div>
+              </div>
+            </div>
           </>
         )}
       </div>
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -2,7 +2,14 @@ import React from "react";
 // import PropTypes from "prop-types";
 
 const Welcome = () => {
-  return <div style={{ fontSize: "4rem" }}>Welcome to this example app !</div>;
+  return (
+    <div style={{ width: "100%" }}>
+      <div style={{ fontSize: "4rem", textAlign: "center" }}>
+        Welcome to this example app !
+      </div>
+      ;
+    </div>
+  );
 };
 
 Welcome.propTypes = {};
